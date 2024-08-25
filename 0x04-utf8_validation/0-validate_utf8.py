017f5a6ef718ec1890b95e50b7c75e3641a0fa12
kassem00 <kamsdonga@gmail.com>
2024-08-25 20:19:46 +0000

    au

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -18,25 +18,29 @@ def validUTF8(data):
     Returns:
         bool: True if data is a valid UTF-8 encoding, else False.
     """
-    n_bytes = 0
     for i in data:
         if i > 255:
-            return False
-        bin_rep = format(i, '08b')
+            return False  # Invalid byte value
+
+        bin_rep = format(i, '08b')  # Get the binary representation
+
         if n_bytes == 0:
             if bin_rep[0] == '0':
-                continue
+                continue  # 1-byte character
             elif bin_rep[:3] == '110':
-                n_bytes = 1
+                n_bytes = 1  # 2-byte character
             elif bin_rep[:4] == '1110':
-                n_bytes = 2
+                n_bytes = 2  # 3-byte character
             elif bin_rep[:5] == '11110':
-                n_bytes = 3
+                n_bytes = 3  # 4-byte character
             else:
-                return False
+                return False  # Invalid start byte
         else:
+            # Continuation byte must start with '10'
             if bin_rep[:2] != '10':
                 return False
-        n_bytes -= 1
 
+        n_bytes -= 1  # Decrease the number of bytes to process
+
+    # If n_bytes is not 0, then we have an incomplete character
     return n_bytes == 0
