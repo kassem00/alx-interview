9df4a2f4c48e20becd8391d60c653bd908bc16f9
kassem00 <kamsdonga@gmail.com>
2024-09-09 02:24:13 +0000

    au

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,37 @@
 #!/usr/bin/node
 
-const request = require('request-promise-native');
+const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-(async () => {
-  try {
-    const filmData = await request(apiUrl);
-    const characters = JSON.parse(filmData).characters;
-
-
-    for (const characterUrl of characters) {
-      const characterData = await request(characterUrl);
-      console.log(JSON.parse(characterData).name);
-    }
-  } catch (error) {
+request(apiUrl, (error, response, body) => {
+  if (error) {
     console.error(error);
+    return;
   }
-})();
+
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+
+  // Fetch each character's name in order using a loop
+  printCharacters(characters, 0);
+});
+
+function printCharacters(characters, index) {
+  if (index >= characters.length) {
+    return; // End of the list, stop recursion
+  }
+
+  request(characters[index], (charError, charResponse, charBody) => {
+    if (charError) {
+      console.error(charError);
+      return;
+    }
+
+    const characterData = JSON.parse(charBody);
+    console.log(characterData.name);
+
+    // Call the function recursively for the next character
+    printCharacters(characters, index + 1);
+  });
+}
