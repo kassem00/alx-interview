aba51629fa3313cc7230c3dac03b385e18b9e18d
kassem00 <kamsdonga@gmail.com>
2024-09-12 06:19:59 +0000

    au

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,16 +3,17 @@
 Rotate a 2D matrix 90 degrees clockwise in place.
 """
 
+
 def rotate_2d_matrix(matrix):
-    N = len(matrix[0])
     """
     Rotates a given NxN 2D matrix 90 degrees clockwise in place.
     :param matrix: 2D list of integers representing the matrix
     """
+    N = len(matrix)
     for x in range(0, int(N / 2)):
-        for y in range(x, N-x-1):
+        for y in range(x, N - x - 1):
             temp = matrix[x][y]
-            matrix[x][y] = matrix[y][N-1-x]
-            matrix[y][N-1-x] = matrix[N-1-x][N-1-y]
-            matrix[N-1-x][N-1-y] = matrix[N-1-y][x]
-            matrix[N-1-y][x] = temp
+            matrix[x][y] = matrix[N - 1 - y][x]
+            matrix[N - 1 - y][x] = matrix[N - 1 - x][N - 1 - y]
+            matrix[N - 1 - x][N - 1 - y] = matrix[y][N - 1 - x]
+            matrix[y][N - 1 - x] = temp
