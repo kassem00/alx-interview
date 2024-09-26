d95619744f535f8d3f17e6dc0e951e13d51e0dba
kassem00 <kamsdonga@gmail.com>
2024-09-26 16:21:30 +0000

    au

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,42 +13,31 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                perimeter = perimeter + posetion(grid, row, col)
+                pram = 4
+                max_left = len(grid[0])
+                max_right = 0
+                max_up = 0
+                max_down = len(grid)
+
+                # Check left
+                if col != max_right:
+                    if grid[row][col - 1] == 1:
+                        pram -= 1
+
+                # Check right
+                if col != max_left:
+                    if grid[row][col + 1] == 1:
+                        pram -= 1
+
+                # Check up
+                if row != max_up:
+                    if grid[row - 1][col] == 1:
+                        pram -= 1
+
+                # Check down
+                if row != max_down:
+                    if grid[row + 1][col] == 1:
+                        pram -= 1
+
+                perimeter += pram
     return perimeter
-
-
-def posetion(grid, row, column):
-    """
-    it looks around the element and find one
-    every time it found 1 a round island it will -1
-    from value of pram it asume first no one around island
-    and return the pram
-    nume calculated
-    """
-    pram = 4
-    max_left = len(grid[0])
-    max_right = 0
-    max_up = 0
-    max_down = len(grid)
-    dirction = ['r', 'l', 'u', 'd']
-
-    for d in dirction:
-        if d == 'r':
-            if column != max_right:
-                if grid[row][column - 1] == 1:
-                    pram -= 1
-
-        if d == 'l':
-            if column != max_left:
-                if grid[row][column + 1] == 1:
-                    pram -= 1
-
-        if d == 'u':
-            if row != max_up:
-                if grid[row - 1][column] == 1:
-                    pram -= 1
-        if d == 'd':
-            if row != max_down:
-                if grid[row + 1][column] == 1:
-                    pram -= 1
-    return pram
