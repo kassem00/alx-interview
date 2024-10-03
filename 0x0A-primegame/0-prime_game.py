2ee9a787cd58dc6b117a98b840cc9080f5b05df9
kassem00 <kamsdonga@gmail.com>
2024-10-03 21:36:40 +0000

    au

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,5 +7,5 @@ def isWinner(x, nums):
     """ is int """
     # x: the number of rounds
     # nums: a list containing the value of n for each round
-    # Return: the name of the player who wins the most rounds or None if it's a tie
+    # Return: the name of the player who wins the most rounds
     return None
